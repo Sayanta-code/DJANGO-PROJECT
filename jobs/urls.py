a5ad45391a39f305009db51a3853bb93dbdea5ea
sayantan chakraborty <sayantan.chkraborty@tuteck.com>
2024-08-05 11:51:25 +0530

    updated code

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,6 +3,7 @@ from .views import product_list, product_data,upload_excel
 
 urlpatterns = [
     path('products/', product_list, name='product_list'),
+     path('/', product_list, name='product_list'),
     path('products/data/', product_data, name='product_data'),
      path('products/upload_excel/', upload_excel, name='upload_excel'),
 ]
